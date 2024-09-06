9618336577edf39b1eeec6cd2cf69020755253d0
Jayant <jjayant511@gmail.com>
2024-09-05 18:06:21 -0700

    New Changes2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,22 +1,23 @@
 class SessionsController < ApplicationController
-    skip_before_action :verify_authenticity_token, only: [:create]
+  skip_before_action :authenticate_user!, only: [:create]
+  skip_before_action :verify_authenticity_token, only: [:create]
 
-    def create
-      user = User.find_by(email: params[:email])
-  
-      if user && user.authenticate(params[:password])
-        token = encode_token(user_id: user.id)
-        render json: { token: token }, status: :ok
-      else
-        render json: { error: 'Invalid email or password' }, status: :unauthorized
-      end
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      token = encode_token(user_id: user.id)
+      render json: { token: token }, status: :ok
+    else
+      render json: { error: 'Invalid email or password' }, status: :unauthorized
     end
-  
-    private
-  
-    def encode_token(payload)
-      secret_key = Rails.application.credentials.secret_key_base
-      JWT.encode(payload, secret_key, 'HS256')
-    end
+  end
+
+  private
+
+  def encode_token(payload)
+    secret_key = Rails.application.credentials.secret_key_base
+    JWT.encode(payload, secret_key, 'HS256')
+  end
 end
 
