6f754467287ad05fd820d3db79820f0dc1efbcb5
jayant jain <jjayant825@gmail.com>
2024-10-05 12:03:01 -0400

    add Comments

--- db/migrate/20241004145138_add_creator_id_to_projects.rb
+++ db/migrate/20241004145138_add_creator_id_to_projects.rb
@@ -1,5 +1,8 @@
 class AddCreatorIdToProjects < ActiveRecord::Migration[7.2]
   def change
+
+    # Adds a project_creator_id column to the projects table
+    # to associate a project with its creator.
     add_column :projects, :project_creator_id, :integer
   end
 end
