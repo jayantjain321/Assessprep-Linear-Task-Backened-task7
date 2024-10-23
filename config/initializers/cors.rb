215a5de0d81548dfc57143856fa8010b5e5bc9a2
jayant jain <jjayant825@gmail.com>
2024-10-23 13:25:45 -0400

    add cors for local

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://linear-iota.vercel.app'  # Change this to your frontend's origin
+    origins 'http://localhost:3001'  # Change this to your frontend's origin
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
